880810c08a2ff50ac4a5a6c0a98eec1f36fe6837
Sonicrida <434238+Sonicrida@users.noreply.github.com>
2019-02-15 23:32:15 -0700

    Use hooks lol

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Formik } from 'formik';
 import { Box, Button, FormField, Heading, TextInput } from 'grommet';
 
-class Home extends Component {
-	state = { submitted: false };
+const Home = () => {
+	const [submitted, setSubmitted] = useState(false);
+	console.log(submitted);
 
-	render() {
-		const { submitted } = this.state;
-		return (
-			<Box align="center">
-				<Box margin="large" elevation="medium" pad="medium" round="medium">
-					<Heading
-						margin={{
-							top: '0'
-						}}
-					>
-						Create A Donation Page
-					</Heading>
-					<Formik
-						validate={values => {
-							const errors = {};
-							if (!values.address) {
-								errors.address = 'required';
-							}
-							if (!values.name) {
-								errors.name = 'required';
-							}
-							if (!values.username) {
-								errors.username = 'required';
-							}
+	return (
+		<Box align="center">
+			<Box margin="large" elevation="medium" pad="medium" round="medium">
+				<Heading
+					margin={{
+						top: '0'
+					}}
+				>
+					Create A Donation Page
+				</Heading>
+				<Formik
+					validate={values => {
+						const errors = {};
+						if (!values.address) {
+							errors.address = 'required';
+						}
+						if (!values.name) {
+							errors.name = 'required';
+						}
+						if (!values.username) {
+							errors.username = 'required';
+						}
+						console.log(errors);
 
-							return errors;
-						}}
-						validateOnBlur={submitted}
-						validateOnChange={submitted}
-						onSubmit={(values, { setSubmitting }) => {
-							alert('Submitting\n' + JSON.stringify(values, null, 2));
-							setSubmitting();
-						}}
-					>
-						{({ values, errors, handleChange, handleSubmit, setFieldValue }) => (
-							<form
-								onSubmit={event => {
-									event.preventDefault();
-									this.setState({ submitted: true });
-									handleSubmit();
-								}}
-							>
-								<FormField label="Address" error={errors.address}>
-									<TextInput name="address" value={values.address || ''} onChange={handleChange} />
-								</FormField>
-								<FormField label="Name" error={errors.name}>
-									<TextInput name="name" value={values.name || ''} onChange={handleChange} />
-								</FormField>
-								<FormField label="Username" error={errors.username}>
-									<TextInput name="username" value={values.username || ''} onChange={handleChange} />
-								</FormField>
+						return errors;
+					}}
+					validateOnBlur={submitted}
+					validateOnChange={submitted}
+					onSubmit={(values, { setSubmitting }) => {
+						alert('Submitting\n' + JSON.stringify(values, null, 2));
+						setSubmitting();
+					}}
+				>
+					{({ values, errors, handleChange, handleSubmit, setFieldValue }) => (
+						<form
+							onSubmit={event => {
+								event.preventDefault();
+								setSubmitted(true);
+								handleSubmit();
+							}}
+						>
+							<FormField label="Address" error={errors.address}>
+								<TextInput name="address" value={values.address || ''} onChange={handleChange} />
+							</FormField>
+							<FormField label="Name" error={errors.name}>
+								<TextInput name="name" value={values.name || ''} onChange={handleChange} />
+							</FormField>
+							<FormField label="Username" error={errors.username}>
+								<TextInput name="username" value={values.username || ''} onChange={handleChange} />
+							</FormField>
 
-								<Box tag="footer" margin={{ top: 'medium' }} direction="row" justify="between">
-									<Button label="Cancel" />
-									<Button type="submit" primary label="Create" />
-								</Box>
-							</form>
-						)}
-					</Formik>
-				</Box>
+							<Box tag="footer" margin={{ top: 'medium' }} direction="row" justify="between">
+								<Button label="Cancel" />
+								<Button type="submit" primary label="Create" />
+							</Box>
+						</form>
+					)}
+				</Formik>
 			</Box>
-		);
-	}
-}
+		</Box>
+	);
+};
 
 export default Home;
