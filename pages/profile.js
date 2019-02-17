23e4f550af51e5e60da565c56d07b124b46fca09
Sonicrida <434238+Sonicrida@users.noreply.github.com>
2019-02-17 02:59:05 -0700

    Show passphrase if one is stored in localstorage

--- pages/profile.js
+++ pages/profile.js
@@ -1,36 +1,59 @@
-import { Box, Heading, Text } from "grommet";
-import React from "react";
+import { Box, Heading, Text, Collapsible, Button } from "grommet";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "next/router";
 import fetch from "isomorphic-unfetch";
 
-const Profile = withRouter(props => (
-  <Box align="center">
-    <Box
-      margin="large"
-      width="large"
-      elevation="medium"
-      pad="medium"
-      round="medium"
-    >
-      <Heading margin={{ top: "0" }}>
-        {props.name}
-        <br />
-        <small>{props.username}</small>
-      </Heading>
-      <Text>{props.address}</Text>
-      <p>{props.description}</p>
+const Profile = withRouter(props => {
+  const [phraseDisplay, setPhraseDisplay] = useState(true);
+
+  return (
+    <Box align="center">
+      <Box
+        margin="large"
+        width="large"
+        elevation="medium"
+        pad="medium"
+        round="medium"
+      >
+        <Heading margin={{ top: "0" }}>
+          {props.name}
+          <br />
+          <small>{props.username}</small>
+        </Heading>
+        <Text>{props.address}</Text>
+        <p>{props.description}</p>
+      </Box>
+      <Collapsible open={phraseDisplay}>
+        {props.passphrase && (
+          <Box elevation="medium" pad="medium" round="medium">
+            Passphrase
+            <Button onClick={() => setPhraseDisplay(false)} label="Close" />
+          </Box>
+        )}
+      </Collapsible>
     </Box>
-  </Box>
-));
+  );
+});
 
 Profile.getInitialProps = async function({ query }) {
   const res = await fetch("http://68.183.170.113:8080/data/" + query.username);
   const data = await res.json();
-  console.log(data);
   if (!data.success) {
     return { name: "Profile Not Found" };
   }
   const { name, address, username, description } = data.data;
+  if (process.browser) {
+    if (localStorage.getItem("username") === username) {
+      const passphrase = localStorage.getItem("passphrase");
+      return {
+        name,
+        address,
+        username,
+        description,
+        passphrase
+      };
+    }
+  }
 
   return {
     name,
