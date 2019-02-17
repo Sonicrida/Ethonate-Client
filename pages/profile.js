73a463c9b7a8c9e017a9564018fa4f398329fac3
Sonicrida <434238+Sonicrida@users.noreply.github.com>
2019-02-16 20:00:15 -0700

    Add baasic error handling when profile does not exist

--- pages/profile.js
+++ pages/profile.js
@@ -1,33 +1,43 @@
-import { Box, Heading, Text } from 'grommet';
-import React from 'react';
-import { withRouter } from 'next/router';
-import fetch from 'isomorphic-unfetch';
+import { Box, Heading, Text } from "grommet";
+import React from "react";
+import { withRouter } from "next/router";
+import fetch from "isomorphic-unfetch";
 
 const Profile = withRouter(props => (
-	<Box align="center">
-		<Box margin="large" width="large" elevation="medium" pad="medium" round="medium">
-			<Heading margin={{ top: '0' }}>
-				{props.name}
-				<br />
-				<small>{props.username}</small>
-			</Heading>
-			<Text>0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520</Text>
-			<p>{props.description}</p>
-		</Box>
-	</Box>
+  <Box align="center">
+    <Box
+      margin="large"
+      width="large"
+      elevation="medium"
+      pad="medium"
+      round="medium"
+    >
+      <Heading margin={{ top: "0" }}>
+        {props.name}
+        <br />
+        <small>{props.username}</small>
+      </Heading>
+      <Text>{props.address}</Text>
+      <p>{props.description}</p>
+    </Box>
+  </Box>
 ));
 
 Profile.getInitialProps = async function({ query }) {
-	const res = await fetch('http://68.183.170.113:8080/data/' + query.username);
-	const data = await res.json();
-	const { name, address, username, description } = data.data;
+  const res = await fetch("http://68.183.170.113:8080/data/" + query.username);
+  const data = await res.json();
+  console.log(data);
+  if (!data.success) {
+    return { name: "Profile Not Found" };
+  }
+  const { name, address, username, description } = data.data;
 
-	return {
-		name,
-		address,
-		username,
-		description
-	};
+  return {
+    name,
+    address,
+    username,
+    description
+  };
 };
 
 export default Profile;
