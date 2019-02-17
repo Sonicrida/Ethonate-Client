d79d5a6e892b8a28cec3118ddde6309099809325
Sonicrida <434238+Sonicrida@users.noreply.github.com>
2019-02-16 19:25:14 -0700

    Get data pulled in from api

--- pages/profile.js
+++ pages/profile.js
@@ -1,18 +1,33 @@
 import { Box, Heading, Text } from 'grommet';
 import React from 'react';
 import { withRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
 
 const Profile = withRouter(props => (
 	<Box align="center">
-		<Box margin="large" elevation="medium" pad="medium" round="medium">
+		<Box margin="large" width="large" elevation="medium" pad="medium" round="medium">
 			<Heading margin={{ top: '0' }}>
-				John Doe
+				{props.name}
 				<br />
-				<small>{props.router.query.username}</small>
+				<small>{props.username}</small>
 			</Heading>
 			<Text>0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520</Text>
+			<p>{props.description}</p>
 		</Box>
 	</Box>
 ));
+
+Profile.getInitialProps = async function({ query }) {
+	const res = await fetch('http://68.183.170.113:8080/data/' + query.username);
+	const data = await res.json();
+	const { name, address, username, description } = data.data;
+
+	return {
+		name,
+		address,
+		username,
+		description
+	};
+};
 
 export default Profile;
