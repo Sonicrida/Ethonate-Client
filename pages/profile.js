96801e32ce90653616f5f474e85e86f5bf94bdba
Sonicrida <434238+Sonicrida@users.noreply.github.com>
2019-02-16 18:00:46 -0700

    Configure dev server for dynamic routing

--- pages/profile.js
+++ pages/profile.js
@@ -1,17 +1,18 @@
-import { Box, Heading, Text } from "grommet";
-import React from "react";
+import { Box, Heading, Text } from 'grommet';
+import React from 'react';
+import { withRouter } from 'next/router';
 
-const Profile = () => (
-  <Box align="center">
-    <Box margin="large" elevation="medium" pad="medium" round="medium">
-      <Heading margin={{ top: "0" }}>
-        John Doe
-        <br />
-        <small>john</small>
-      </Heading>
-      <Text>0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520</Text>
-    </Box>
-  </Box>
-);
+const Profile = withRouter(props => (
+	<Box align="center">
+		<Box margin="large" elevation="medium" pad="medium" round="medium">
+			<Heading margin={{ top: '0' }}>
+				John Doe
+				<br />
+				<small>{props.router.query.username}</small>
+			</Heading>
+			<Text>0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520</Text>
+		</Box>
+	</Box>
+));
 
 export default Profile;
